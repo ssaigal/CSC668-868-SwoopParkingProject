160e10db4269ef49e76a3845397b101f0da9d89e
Shalaka Aigal <shalaka.aigal@gmail.com>
2018-05-10 09:38:11 -0700

    Cross Origin connection to React Port 3000

--- swoop-backend/src/main/java/com/lostsemicolon/swoop/UserController.java
+++ swoop-backend/src/main/java/com/lostsemicolon/swoop/UserController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 @RestController
 @RequestMapping({"/api"})
 public class UserController {
